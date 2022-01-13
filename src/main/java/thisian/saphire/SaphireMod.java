b37d9eed99424472c1c34cd946e9a377a6b3f9a6
K Nguyen <ThisIan>
2022-01-13 12:09:00 -0600

    first commit

---
+++ src/main/java/thisian/saphire/SaphireMod.java
@@ -0,0 +1,19 @@
+package thisian.saphire;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SaphireMod implements ModInitializer {
+
+    public static final String MOD_ID = "saphire";
+    public static final Logger LOGGER = LogManager.getLogger("modid");
+
+    @Override
+    public void onInitialize() {
+
+
+        LOGGER.info("PP");
+    }
+
+}
